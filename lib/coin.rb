c283cb3606516a748a9fc433d9af23340738d523
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-07-26 15:16:35 +0900

    add coin factory

--- lib/coin.rb
+++ lib/coin.rb
@@ -36,3 +36,31 @@ class Price1000 < CoinBase
   end
 end
 
+class CoinFactory
+  def self.Coins(coin_prices)
+    coins = []
+    for price in coin_prices
+
+    end
+    coins
+  end
+
+  def self.Coin(price)
+    coin = nil
+    case price
+    when 10 then
+      coin = Price10.new
+    when 50 then
+      coin = Price50.new
+    when 100 then
+      coin = Price100.new
+    when 500 then
+      coin = Price500.new
+    when 1000 then
+      coin = Price1000.new
+    else
+      raise "You need price #{price} coin is not uses."
+    end
+    coin
+  end
+end
