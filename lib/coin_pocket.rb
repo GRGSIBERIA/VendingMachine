479d9730bdb69d60a63697b401869a908968104f
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-07-26 13:18:15 +0900

    rename coin_pocket to coin_depot

--- lib/coin_pocket.rb
+++
@@ -1,3 +0,0 @@
-class CoinPocket
-
-end
--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -2,11 +2,13 @@ require "./lib/coin.rb"
 require "./lib/stamp.rb"
 require "./lib/buyer.rb"
 require "./lib/refunder.rb"
+require "./lib/coin_depot.rb"
 
 class VendingMachine
   def initialize
     @buyer
     @refunder
+    @coin_depot
   end
 
   def Buy(coins)
