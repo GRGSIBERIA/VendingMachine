61f229d85688b4faf4b0351b8f35287be50f801e
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-07-26 13:23:09 +0900

    ad deposit and withdraw

--- lib/coin_depot.rb
+++ lib/coin_depot.rb
@@ -1,3 +1,15 @@
-class CoinDepot
+require "./lib/coin.rb"
 
+class CoinDepot
+  def initialize(initial_coins)
+    @coins = initial_coins
+  end
+
+  def Deposit
+
+  end 
+
+  def Withdraw
+
+  end
 end
