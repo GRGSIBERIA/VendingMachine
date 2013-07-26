31ee0e0c3a0cf2f81ec0fb500b1f5ad8ddc482dc
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-07-26 16:17:12 +0900

    add

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,21 +1,27 @@
 require "./lib/coin.rb"
 require "./lib/stamp.rb"
-require "./lib/buyer.rb"
-require "./lib/refunder.rb"
 require "./lib/coin_depot.rb"
+require "./lib/stamp_depot.rb"
+require "./lib/price_counter.rb"
 
 class VendingMachine
   def initialize
-    @buyer
-    @refunder
-    @coin_depot
+    @coin_depot = CoinDepot.new(
+      [10] * 40 + [50] * 20 + [100] * 10 + [500] * 5)
+    @stamp_depot = StampDepot.new(20, 20)
+    @counter = PriceCounter.new
   end
 
-  def Buy(coins)
+  def insert(coin_price)
+    coin = CoinFactory.coin(coin_price)
+    @counter.insert(coin)
+  end
+
+  def buy(stamp_price)
 
   end
 
-  def Refund
-
+  def refund
+    @counter.refund
   end
 end
