84aa5918aaccb8db0fff5fc3a7878babbe294f93
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-07-26 12:53:50 +0900

    add constructor

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,23 +1,37 @@
 class CoinBase
+  def initialize(price)
+    @price = price
+  end
 
+  attr_reader :price
 end
 
 class Price10 < CoinBase
-
+  def initialize
+    super(10)
+  end
 end
 
 class Price50 < CoinBase
-
+  def initialize
+    super(50)
+  end
 end
 
 class Price100 < CoinBase
-
+  def initialize
+    super(100)
+  end
 end
 
 class Price500 < CoinBase
-
+  def initialize
+    super(500)
+  end
 end
 
 class Price1000 < CoinBase
-
+  def initialize
+    super(1000)
+  end
 end
