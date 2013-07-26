dc7874cb1a1b6b0a62bb74ff1c5fef38447e7919
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-07-26 15:53:50 +0900

    add stamp depot

--- lib/stamp_depot.rb
+++ lib/stamp_depot.rb
@@ -1,5 +1,17 @@
 require "./lib/stamp.rb"
 
 class StampDepot
-  
+  def initialize(stamp50_number, stamp80_number)
+    @stamps = Hash.new
+    @stamps[50] = [Stamp50.new] * stamp50_number
+    @stamps[80] = [Stamp80.new] * stamp80_number
+  end
+
+  def Withdraw(price_of_stamp)
+    begin
+      return @stamps[price_of_stamp].pop
+    rescue
+      raise "You need price #{price_of_stamp} stamp is not exists."
+    end
+  end
 end
