07f90f4858c869a757412cc32e99482a93ff1834
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-07-26 12:51:30 +0900

    add coin

---
+++ lib/coin.rb
@@ -0,0 +1,23 @@
+class CoinBase
+
+end
+
+class Price10 < CoinBase
+
+end
+
+class Price50 < CoinBase
+
+end
+
+class Price100 < CoinBase
+
+end
+
+class Price500 < CoinBase
+
+end
+
+class Price1000 < CoinBase
+
+end
