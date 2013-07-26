5db6c413a963725d2045dcf6a21a6553df61497e
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-07-26 13:10:23 +0900

    add buyer

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,9 +1,12 @@
 require "./lib/coin.rb"
 require "./lib/stamp.rb"
+require "./lib/buyer.rb"
+require "./lib/refunder.rb"
 
 class VendingMachine
   def initialize
-
+    @buyer
+    @refunder
   end
 
   def Buy(coins)
